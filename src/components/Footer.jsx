cf43e1f689ac4ddf7271597f06782144bfe4c047
ayodeji <ayodejiakinola@gmail.com>
2024-07-06 14:30:29 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="py-12">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className=''>
             <h3 className="uppercase text-sm mb-4">Contact Us</h3>
