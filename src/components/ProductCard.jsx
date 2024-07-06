f4d9b24e374ca9c35f56f30852a1ba47fafc1530
ayodeji <ayodejiakinola@gmail.com>
2024-07-06 13:19:45 +0100

    h

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 const ProductCard = ({ product }) => {
   const { img, title, price } = product;
   return (
-    <div className="max-w-xs bg-white rounded-lg shadow-lg overflow-hidden group transition-all duration-300 transform hover:scale-105">
+    <div className="container max-w-xs bg-white rounded-lg shadow-lg overflow-hidden group transition-all duration-300 transform hover:scale-105">
       <div className="relative overflow-hidden">
-        <img className="w-full h-48 object-cover object-center group-hover:scale-110 transition-transform duration-500 cursor-pointer" src={img} alt={title} />
+        <img className="w-full h-48 object-cover object-center group-hover:scale-110 transition-transform duration-500" src={img} alt={title} />
         <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500">
           <div className="absolute bottom-4 left-4 flex space-x-2">
             <button className="flex items-center justify-center text-white w-8 h-8 bg-[#C19A6B] rounded-full">
